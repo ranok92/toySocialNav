fc5b15a7df27d262b95607863f778683f0346fd2
Abhisek KOnar <konarabhisek@gmail.com>
2018-12-06 12:51:53 -0500

    Checkpoint for december 06

--- toy_car_IRL.py
+++ toy_car_IRL.py
@@ -6,6 +6,7 @@ import scipy
 #import ballenv_pygame as BE
 from playing import play   #get the RL Test agent, gives out feature expectations after 2000 frames
 from playing import test_model
+from playing import play_User
 from nn import Neural_net as neural_net #construct the nn and send to playing
 from cvxopt import matrix 
 from cvxopt import solvers #convex optimization library
@@ -19,18 +20,18 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 NUM_STATES = 8 
 BEHAVIOR = 'red' # yellow/brown/red/bumping
-FRAMES = 1000 # number of RL training frames per iteration of IRL
-
+FRAMES = 10000 # number of RL training frames per iteration of IRL
+ENVIRONMENTSIZE = 100
 class irlAgent:
-    def __init__(self, randomFE, expertTrajFile, epsilon, num_states, num_frames, behavior):
+    def __init__(self, randomFE, expertFE, epsilon, num_states, num_frames, behavior , featsize):
         self.randomPolicy = randomFE
-        self.expertTrajFile = expertTrajFile #directory that contains the expert trajetories
+        #self.expertTrajFile = expertTrajFile #directory that contains the expert trajetories
 
         # The trajectories are of the of the format : a list of array, where each array contains
         # the state information at each time of the trajectory
-        self.expertPolicy = None
-        self.getExpertFE()
-
+        self.expertPolicy = expertFE
+        #self.getExpertFE = expertFE
+        self.IRLFeaureSize = featsize
         self.num_states = num_states
         self.num_frames = num_frames
         self.behavior = behavior
@@ -41,21 +42,26 @@ class irlAgent:
         self.currentT = self.randomT
         self.minimumT = self.randomT
         self.weightdistanceList = []
-
+        self.nn_param = [512, 1024 , 128]
+        self.params = {
+        "batchSize": 100,
+        "buffer": 50000,
+        "nn": nn_param
+        }
 
 
         self.fig = plt.figure()
         self.ax1 = self.fig.add_subplot(1,1,1)
 
    #given the file name, reads the dictionary created by eth_util.py and upadates the expertFE in the form of a vector.
-
+    #this method takes expert trajectory from 3rd party tracking videos.
     def getExpertFE(self):
 
         gamma = .9
         trajFilelist = None
         expertFE = np.zeros([44])
         counter = np.zeros([44])
-        print expertFE.size
+        print expertFE.sizeFRAMES
         if not os.path.isdir(self.expertTrajFile):
 
             print ("The trajectories do not exist.")
@@ -67,18 +73,18 @@ class irlAgent:
 
         totaltraj = len(trajFilelist)
         print "started"
-        for i in range(len(trajFilelist)):
+        for i in range(len(trajFilelist)): #looping through all the trajectory files
             #print i
             timestep = 0
             singleRunFE = np.zeros([44])
             filename = os.path.join(self.expertTrajFile,trajFilelist[i])
             #print filename
-            with open(filename , 'r') as f:
-                for line in f:
+            with open(filename , 'r') as f:  #opening a single file of trajectory
+                for line in f:              #looping through the lines of a single trajectory file
                     state_list = line.strip().split(',')
                     state_arr = np.array(state_list)
                     state_arr = state_arr.astype(np.float)
-                    for i in range(state_arr.size):
+                    for i in range(state_arr.size):  #looping through the values of a single sensor reading array
                         if not math.isnan(state_arr[i]):
                             singleRunFE[i] = singleRunFE[i]+ (math.pow(gamma,timestep)*state_arr[i])
                             counter[i]+=1
@@ -92,12 +98,14 @@ class irlAgent:
 
     def getRLAgentFE(self, W, i): #get the feature expectations of a new poliicy using RL agent
 
-        IRL_helper(W, self.behavior, self.num_frames, i) # train the agent and save the model in a file used below
+        #IRL_helper() - returns the filename in which the state dictionary of the trained model is stored
+        model_dict_file = IRL_helper(W, self.behavior, self.num_frames, i , self.IRLFeaureSize) # train the agent and save the model in a file used below
         #saved_model = 'saved-models_'+self.behavior+'/evaluatedPolicies/'+str(i)+'-164-150-100-50000-'+str(self.num_frames)+'.h5' # use the saved model to get the FE
         #model = neural_net(self.num_states, [164, 150], saved_model)
 
         # *** no saved model as of now ***
-        return  play(W,saved_model = None)#return feature expectations by executing the learned policy
+        print "Model dict :",model_dict_file
+        return  play(W,saved_model = model_dict_file , nnParams=self.nn_param , featsize = self.IRLFeaureSize)#return feature expectations by executing the learned policy
 
     def get_saved_model_FileName(self):
 
@@ -113,14 +121,18 @@ class irlAgent:
         return hyperDistance # t = (weights.tanspose)*(expert-newPolicy)
         
     def optimalWeightFinder(self):
-        f = open('weights-'+BEHAVIOR+'.txt', 'w')
-        i = 1
 
+
+        i = 1
+        MAX_ITERATIONS = 1000
         while True:
             W = self.optimization() # optimize to find new weights in the list of policies
             print ("weights ::", W )
-            f.write( str(W) )
+            f = open('weights-'+BEHAVIOR+'.txt', 'a')
+            writestr =  'Iteration :' + str(i) + ' - ' + str(W)
+            f.write( writestr )
             f.write('\n')
+            f.close()
             print ("the distances  ::", self.policiesFE.keys())
             self.currentT = self.policyListUpdater(W, i)
             print ("Current distance (t) is:: ", self.currentT )
@@ -134,18 +146,22 @@ class irlAgent:
             with open('results/model_paths.txt','r') as mdl:
                 model_path = mdl.read().strip()
                 mdl.close()
-            test_model(W,model_path,i)
-            if self.currentT <= self.epsilon: # terminate if the point reached close enough
+            test_model(W,model_path,i , self.nn_param , sensor_size = self.IRLFeatureSize)
+            if self.currentT <= self.epsilon or i>= MAX_ITERATIONS: # terminate if the point reached close enough
                 break
             i += 1
-        f.close()
+
         return W
 
 
 
 
     def optimization(self): # implement the convex optimization, posed as an SVM problem
-        m = len(self.expertPolicy)
+        print 'expert policy', self.expertPolicy
+        self.expertPolicy = np.squeeze(self.expertPolicy)
+        self.expertPolicy = np.squeeze(self.expertPolicy)
+        m = self.expertPolicy.shape[0]
+        print 'MMMM',type(m)
         P = matrix(2.0*np.eye(m), tc='d') # min ||w||
         q = matrix(np.zeros(m), tc='d')
         policyList = [self.expertPolicy]
@@ -153,42 +169,73 @@ class irlAgent:
         for i in self.policiesFE.keys():
             policyList.append(self.policiesFE[i])
             h_list.append(1)
-        policyMat = np.matrix(policyList)
-        policyMat[0] = -1*policyMat[0]
-        G = matrix(policyMat, tc='d')
-        h = matrix(-np.array(h_list), tc='d')
-        sol = solvers.qp(P,q,G,h)
+        print 'policyList',policyList[0].shape
+        print 'policyList 2', policyList[1].shape
 
+
+
+        policyMat = np.array(policyList)
+        policyMat = policyMat.astype(np.double)
+        print policyMat.dtype
+        #policyMat = policyMat.astype(np.double)
+        policyMat[0] = -1*policyMat[0]
+        G = matrix(policyMat,tc= 'd')
+        h = matrix(-np.array(h_list), tc='d')
+        print "P :",P
+        print "q : ",q
+        print "m : ",m
+        print "G : ",G
+        print "h : ",h
+        sol = solvers.qp(P,q,G,h)
+        print sol['status']
         weights = np.squeeze(np.asarray(sol['x']))
         norm = np.linalg.norm(weights)
         weights = weights/norm
+        plt.bar(range(len(weights)),weights)
+        plt.show(block=False)
         return weights # return the normalized weights
-                
+
+
+#this helper method takes the FE expectation from a player playing the ball environment itsel
+def getExpertFE(NO_OF_TRIALS,nn_params):
+
+
+    return play_User(NO_OF_TRIALS,nn_params)
+
             
 if __name__ == '__main__':
     logger = logging.getLogger()
+
+
+    #to make changes in the featuresize change below
+    #in the main of learning.py line 15
+
+    FEATSIZE = 5
     logger.setLevel(logging.INFO)
     #randomPolicyFE = [ 7.74363107 , 4.83296402 , 6.1289194  , 0.39292849 , 2.0488831  , 0.65611318 , 6.90207523 , 2.46475348]
-
-    intiWeights = np.random.rand(44)
-    randomPolicyFE = play(intiWeights)
+    nn_param = [32, 128 ,16]
+    params = {
+        "batchSize": 100,
+        "buffer": 50000,
+        "nn": nn_param
+    }
+    expertFE = np.asarray([np.genfromtxt('newFTFile2_1feat.txt')])
+    #expertFE = np.divide(expertFE) #500 because size of the current environment is 500 x 500
+    intiWeights = np.random.rand(FEATSIZE)
+    randomPolicyFE = play(intiWeights , nnParams= nn_param , featsize= FEATSIZE)
     print "Generated random FE"
-    # ^the random policy feature expectations
-    expertPolicyYellowFE = [7.5366e+00,  4.6350e+00  , 7.4421e+00, 3.1817e-01,  8.3398e+00,  1.3710e-08,  1.3419e+00 ,  0.0000e+00]
-    # ^feature expectations for the "follow Yellow obstacles" behavior
-    expertPolicyRedFE = [7.9100e+00, 5.3745e-01,  5.2363e+00, 2.8652e+00,  3.3120e+00, 3.6478e-06, 3.82276074e+00  , 1.0219e-17] 
-    # ^feature expectations for the follow Red obstacles behavior
-    expertPolicyBrownFE = [5.2210e+00,  5.6980e+00,  7.7984e+00,  4.8440e-01, 2.0885e-04, 9.2215e+00, 2.9386e-01 , 4.8498e-17]
-    # ^feature expectations for the "follow Brown obstacles" behavior
-    expertPolicyBumpingFE = [  7.5313e+00, 8.2716e+00, 8.0021e+00, 2.5849e-03 ,2.4300e+01 ,9.5962e+01 ,1.5814e+01 ,1.5538e+03]
-    # ^feature expectations for the "nasty bumping" behavior
-    
-    trajdir = "expert_traj"
-    epsilon = 0.1
+
+    print "Expert FE :",expertFE
+    #trajdir = "expert_traj2"
+    epsilon = .001
     print "Start agent"
     if torch.device("cuda"):
         print "Using CUDA"
-    irlearner = irlAgent(randomPolicyFE, trajdir  , epsilon, NUM_STATES, FRAMES, BEHAVIOR)
-    irlearner.getExpertFE()
+
+    trajdir = None
+    print "randompolicy", randomPolicyFE
+    print "expertpolicy", expertFE
+    irlearner = irlAgent(randomPolicyFE, expertFE  , epsilon, NUM_STATES, FRAMES, BEHAVIOR , FEATSIZE)
+    #irlearner.getExpertFE()
     print (irlearner.optimalWeightFinder())
 
