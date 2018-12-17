53018cb97a67600a746024672a1d5c35dbb5727b
Abhisek KOnar <konarabhisek@gmail.com>
2018-12-17 13:20:04 -0500

    actor critic works for dynamic environment. But agent falls into local troves. Makes sense why.

--- ballgameActorCritic.py
+++ ballgameActorCritic.py
@@ -1,4 +1,5 @@
 import ballenv_pygame as BE
+#from flat_game import ballgamepyg as BE
 import numpy as np
 from itertools import count
 from collections import namedtuple
@@ -11,7 +12,10 @@ import datetime
 from matplotlib import pyplot as plt
 import os
 
-env = BE.createBoard(display= True,static_obstacles=15)
+env = BE.createBoard(display= True, static_obstacles= 3 , static_obstacle_radius= 20)
+AGENT_RAD = 10
+
+
 gamma = .99
 log_interval = 1000
 render = True
@@ -21,9 +25,11 @@ plt.ion()
 
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
-def block_to_arrpos(x,y):
+def block_to_arrpos(window_size,x,y):
 
-    pos = 12+(5*y)+x
+    a = (window_size**2-1)/2
+    b = window_size
+    pos = a+(b*y)+x
     return int(pos)
 
 
@@ -33,9 +39,18 @@ def get_state_BallEnv(state):
 #agent, 2nd the position of the goal , 3rd the distance after that,
 #the positions of the obstacles in the world
     #print(state)
-    ref_state = np.zeros(29)
+    window_size = 5
+    block_width = 2
+
+    window_rows = window_size
+    row_start =  (window_rows-1)/2
+    window_cols = window_size
+    col_start = (window_cols-1)/2
+
+    ref_state = np.zeros(4+window_size**2)
     #print(ref_state.shape)
-    ref_state[12+4] = 1
+    a = (window_size**2-1)/2
+    ref_state[a+4] = 1
     agent_pos = state[0]
     goal_pos = state[1]
     diff_x = goal_pos[0] - agent_pos[0]
@@ -50,25 +65,23 @@ def get_state_BallEnv(state):
         ref_state[2] = 1
 
     for i in range(3,len(state)):
-        x_dist = agent_pos[0] - state[i][0]
-        y_dist = agent_pos[1] - state[i][1]
-        x_block = y_block = 0
-        #print("XY",x_dist,y_dist)
-        if x_dist!=0 and y_dist!=0:
-            if x_dist < 0:
-                x_block = ((x_dist/abs(x_dist))*(x_dist-10)//20)+1
-            else:
-                x_block = (x_dist/abs(x_dist))*(x_dist-10)//20
-            if y_dist < 0:
-                y_block = ((y_dist/abs(y_dist))*(y_dist-10)//20)+1
-            else:
-                y_block = (y_dist/abs(y_dist))*(y_dist-10)//20
-            #print("BLOCK",x_block,y_block)
-        if (abs(x_block)<3 and abs(y_block)<3): #considering a matrix of 5x5 with the agent being present at the center
-        #print(x_block,y_block)
-            pos = block_to_arrpos(x_block, y_block) #pos starts from 0
-            #print("POS",pos)
-            ref_state[4+pos] += 1
+
+        #as of now this just measures the distance from the center of the obstacle
+        #this distance has to be measured from the circumferance of the obstacle
+
+        #new method, simulate overlap for each of the neighbouring places
+        #for each of the obstacles
+        obs_pos = state[i][0:2]
+        obs_rad = state[i][2]
+        for r in range(-row_start,row_start+1,1):
+            for c in range(-col_start,col_start+1,1):
+                #c = x and r = y
+                temp_pos = (agent_pos[0] + c*block_width , agent_pos[1] + r*block_width)
+                if checkOverlap(temp_pos,AGENT_RAD, obs_pos, obs_rad):
+                    pos = block_to_arrpos(window_size,r,c)
+
+                    ref_state[pos]=1
+
     #state is as follows:
         #first - tuple agent position
         #second -
@@ -77,6 +90,19 @@ def get_state_BallEnv(state):
     state = state.unsqueeze(0)
 
     return state
+
+
+#returns true if there is an overlap
+def checkOverlap(obj1Pos,obj1rad, obj2Pos, obj2rad):
+
+    xdiff = obj1Pos[0]-obj2Pos[0]
+    ydiff = obj1Pos[1]-obj2Pos[1]
+
+    if (np.hypot(xdiff,ydiff)-obj1rad-obj2rad) > 0:
+
+        return False
+    else:
+        return True
 
 def agent_action_to_WorldActionSimplified(action):
     if action==0: #move front
@@ -92,31 +118,32 @@ def agent_action_to_WorldActionSimplified(action):
 
 
 class Policy(nn.Module):
-    def __init__(self):
+    def __init__(self, inputSize = 5 , outputSize = 9 , hidden = 128):
         super(Policy, self).__init__()
-        self.affine1 = nn.Linear(29, 512)
-        self.affine2 = nn.Linear(512, 128)
-        self.action_head = nn.Linear(128, 4)
-        self.value_head = nn.Linear(128, 1)
+        self.fc1 = nn.Linear(inputSize, hidden)
+        #self.affine1 = nn.Linear(inputSize,512)
+        #self.affine2 = nn.Linear(512, 128)
+        self.action_head = nn.Linear(hidden, outputSize)
+        self.value_head = nn.Linear(hidden, 1)
 
         self.saved_actions = []
         self.rewards = []
 
     def forward(self, x):
-        x = F.relu(self.affine1(x))
-        x = F.relu(self.affine2(x))
+        x = F.relu(self.fc1(x))
+        #x = F.relu(self.affine2(x))
         action_scores = self.action_head(x)
         state_values = self.value_head(x)
         return F.softmax(action_scores, dim=-1), state_values
 
 
-policy = Policy()
+policy = Policy(inputSize=20)
 policy.cuda()
 optimizer = optim.Adam(policy.parameters(), lr=1e-2)
 eps = np.finfo(np.float32).eps.item()
 
 
-def select_action(state):
+def select_action(state,policy):
     #state = torch.from_numpy(state).float().unsqueeze(0)
     '''
     for x in policy.parameters():
@@ -160,12 +187,62 @@ def finish_episode():
     del policy.rewards[:]
     del policy.saved_actions[:]
 
+def testmodel(modelpath, iterations):
 
+    policy = Policy(inputSize=29)
+    policy.load_state_dict(torch.load(modelpath))
+
+    policy.eval()
+    policy.cuda()
+
+    for i in policy.parameters():
+
+        print i.shape
+    env = BE.createBoard(display=  True , static_obstacles= 40)
+    reward_list = []
+    run_list = []
+
+    fig = plt.figure(1)
+    plt.clf()
+
+    for i in range(iterations):
+
+        state = env.reset()
+        state = get_state_BallEnv(state)
+        #state = env.sensor_readings
+        done = False
+        t = 0
+        while not done and t < 1000:
+            action = select_action(state,policy)
+            t+=1
+            if action != None:
+
+                action = move_list[action]
+
+                state, reward, done, _ = env.step(action)
+                #print 'dist :',state[2]
+                state = get_state_BallEnv(state)
+                #state = env.sensor_readings
+                env.render()
+
+        run_list.append(i)
+        reward_list.append(env.total_reward_accumulated)
+        plt.plot(run_list, reward_list,color='black')
+        plt.draw()
+        plt.pause(.0001)
+        fig.show()
+
+
+#move_list = [(5,5) , (5,-5) , (5, 0) , (0,5) , (0,-5),(0,0) , (-5,5),(-5,0),(-5,-5)]
+move_list = [(1,1) , (1,-1) , (1, 0) , (0,1) , (0,-1),(0,0) , (-1,1),(-1,0),(-1,-1)]
 plot_interval = 10
 log_interval = 10
+
+
 def main():
 
-    filename = 'actorCritic'
+    #****information to store the model
+    filename = 'actorCriticFeaturesFull'
     curDay = str(datetime.datetime.now().date())
     curtime = str(datetime.datetime.now().time())
     basePath = 'saved-models_trainBlock' +'/evaluatedPoliciesTest/'
@@ -175,11 +252,13 @@ def main():
     if os.path.exists(curDir):
         print "YES"
 
+    #******************************
+
 
     state = env.reset()
     rewardList = []
     runList = []
-
+    timeList = []
     fig = plt.figure(1)
     plt.clf()
     print eps
@@ -188,15 +267,18 @@ def main():
         state = env.reset()
         #env.render()
         print 'Starting episode :', i_episode
-        state = get_state_BallEnv(state)
-        for t in range(1000):  # Don't infinite loop while learning
-            action = select_action(state)
+        #state = get_state_BallEnv(state)
+        state = env.sensor_readings
+        for t in range(500):  # Don't infinite loop while learning
+            action = select_action(state,policy)
             #print action
             if action!=None:
-                action = agent_action_to_WorldActionSimplified(action)
-            #print action
+                action = move_list[action]
+                #action = agent_action_to_WorldActionSimplified(action)
+                #print action
                 state, reward, done, _ = env.step(action)
-                state = get_state_BallEnv(state)
+                #state = get_state_BallEnv(state)
+                state = env.sensor_readings
                 if i_episode%log_interval==0:
                     env.render()
                 policy.rewards.append(reward)
@@ -211,7 +293,9 @@ def main():
 
         rewardList.append(env.total_reward_accumulated)
         runList.append(i_episode)
+        timeList.append(float(t)/500)
         plt.plot(runList, rewardList,color='black')
+        plt.plot(runList , timeList , color= 'red')
         plt.draw()
         plt.pause(.0001)
         fig.show()
@@ -232,3 +316,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    #model = '/home/abhisek/Study/Robotics/toySocialNav/saved-models_trainBlock/evaluatedPoliciesTest/2018-12-16/11:40:41.466716/610-actorCritic-610.h5'
+    #testmodel(model,50)
